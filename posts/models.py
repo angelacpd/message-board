14ded4176801dd644cbc4af93dcd162817efb181
angelacpd <angelacrepaldi@gmail.com>
2021-02-07 23:25:36 -0300

    Add string representation to model Post.

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        """String representation of the model."""
+        return str(self.id) + " - " + self.text[:50]
